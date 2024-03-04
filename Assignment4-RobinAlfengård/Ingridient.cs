93ffa3e7d6851757b91747bf7258d5c7697505dc
Robin Alfengård <robin.alfengard@hotmail.com>
2024-03-04 16:24:46 +0100

    add recipe + ingridient class

---
+++ Assignment4-RobinAlfengård/Ingridient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_RobinAlfengård
+{
+    internal class Ingridient
+    {
+        private string nameOfIngridient {  get; set; }
+        private int ammount { get; set; }
+
+        public Ingridient(string nameOfIngridient, int ammount)
+        {
+            this.nameOfIngridient = nameOfIngridient;
+            this.ammount = ammount;
+        }
+    }
+
+
+}
--- Assignment4-RobinAlfengård/obj/Debug/net8.0-windows/Assignment4-RobinAlfengård.AssemblyInfo.cs
+++ Assignment4-RobinAlfengård/obj/Debug/net8.0-windows/Assignment4-RobinAlfengård.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4-RobinAlfengård")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18801c9119fb6db01ee24843b0a48d33898b804a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47a9b7f4f96b3c78053cae3c20660c4ef30499b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment4-RobinAlfengård")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment4-RobinAlfengård")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
